5a42392c99d5106ec0bf4e0c4bc30881e7d1dd50
BerezkinNick <berezkin.nikolay.02@mail.ru>
2021-03-22 15:14:16 +0300

    Create C.cpp

---
+++ Lab7/C.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    float x, M, D;
+    float s =0;
+    float c=0;
+    int k = -1;
+    while (x != 0){
+        k += 1;
+        cin>>x;
+        s += x;
+        c += x*x;}
+    M = s/k;
+    D = c/k-(s/k)*(s/k);
+    cout.precision(3);
+    cout<<fixed<<M<<' '<<D;
+}
